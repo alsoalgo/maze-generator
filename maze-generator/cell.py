b208920c26c7415e483e37811266623d465d952a
alsoalgo <gorbulyaalexandr@gmail.com>
2021-04-14 23:56:37 +0300

    Changes in class method if_exists

--- maze-generator/cell.py
+++ maze-generator/cell.py
@@ -1,38 +1,43 @@
 class Cell:
-    walls = [True, True, True, True]
+    _walls = [True, True, True, True]
+    _maze_size = (0, 0)
+    _position = (0, 0)
 
-    def __if_exists(arguments, variable):
-        if variable in arguments.keys():
+    @staticmethod
+    def if_exists(arguments, variable):
+        if variable in arguments:
             return int(arguments[variable])
         return 0
 
     def __init__(self, **kwargs):
-        arguments = kwargs.items()
-        self.maze_size = (__if_exists(arguments, "n"), __if_exists(arguments, "m"))
-        self.position = (__if_exists(arguments, "i"), __if_exists(arguments, "j"))
-        self.walls = [True, True, True, True] 
+        arguments = dict(kwargs)
+        self._maze_size = (Cell.if_exists(arguments, "n"), 
+                            Cell.if_exists(arguments, "m"))
+        self._position = (Cell.if_exists(arguments, "i"), 
+                            Cell.if_exists(arguments, "j"))
+        self._walls = [True, True, True, True] 
 
     @property
     def position(self):
-        return self.position
+        return self._position
 
     @property
     def maze_size(self):
-        return self.maze_size 
+        return self._maze_size 
 
     @property
     def wall_top(self):
-        return self.walls[0]
+        return self._walls[0]
 
     @property
     def wall_left(self):
-        return self.walls[1]
+        return self._walls[1]
 
     @property
     def wall_bottom(self):
-        return self.walls[2]
+        return self._walls[2]
 
     @property
     def wall_right(self):
-        return self.walls[3] 
-    
+        return self._walls[3] 
+
