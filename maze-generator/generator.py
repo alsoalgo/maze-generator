7894fbd25b59fd5b769746fa4e2694c3692eeb9c
alsoalgo <gorbulyaalexandr@gmail.com>
2021-04-15 18:44:14 +0300

    Added MST algorithm

--- maze-generator/generator.py
+++ maze-generator/generator.py
@@ -33,6 +33,32 @@ class Generator:
                 used_cells[un_i][un_j] = True
                 stack.append(unvisited_neighbours[choose])
         return maze
+    
+    def generate_by_mst(self, maze):
+        size = maze.size
+        current_cell = [0, 0]
+        all_walls = []
+        used_cells = [[False for i in range(size[1])] \
+                        for i in range(size[0])]
+        neighbours = maze.get_neighbours(current_cell)
+        for neighbour in neighbours:
+            all_walls.append([current_cell, [neighbour[0], neighbour[1]]]])
+        while all_walls:
+            choose = randrange(0, len(all_walls))
+            v = all_walls[choose][0]
+            u = all_walls[choose][1]
+            is_used_v = int(used_cells[v[0]][v[1]])
+            is_used_u = int(used_cells[u[0]][u[1]])
+            if sum(is_used_u + is_used_v) == 1:
+                if is_used_v:
+                    v, u = u, v
+                maze.remove_wall(v, u)
+                used_cells[v[0]][v[1]] = True
+                neighbours = maze.get_neighbours(v)
+                for neighbour in neighbours:
+                    all_walls.append([v, [neighbour[0], neighbour[1]]]])
+            all_walls.pop(choose)
+        return maze
 
     def generate(self, maze_, type_="DFS"):
         if type_ == "DFS":
