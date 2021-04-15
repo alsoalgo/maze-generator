e844dfd9b03bb3a975b7b694ed74cc397dcbc453
alsoalgo <gorbulyaalexandr@gmail.com>
2021-04-15 23:08:45 +0300

    implemented solver

--- maze_generator/solver.py
+++ maze_generator/solver.py
@@ -5,7 +5,7 @@ from queue import Queue
 
 class Solver:
     _maze = Maze()
-    path = []
+    _path = []
 
     def __init__(self, maze_=Maze()):
         self._maze = maze_
@@ -25,11 +25,21 @@ class Solver:
         while queue:
             current_cell = queue.get()
             neighbours = self._maze.get_neighbours(current_cell)
-            walls = ["top", "left", "bottom", "right"]
-            for i in range(4):
-                if not getattr(current_cell, "wall_" + walls[i]) \
-                    and not used_cells[neighbours[i][0]][neighbours[i][1]]:
+            for neighbour in neighbours:
+                is_used_n = used_cells[neighbours[i][0]][neighbours[i][1]]
+                is_wall_exists = getattr(current_cell, "wall_" + current_cell.neighbour_type(neighbour))
+                if not is_wall_exists and not is_used_n:
                         queue.put(neighbour)
                         used_cells[neighbour[0]][neighbour[1]] = True
                         came_from[neighbour[0]][neighbour[1]] = [current_cell[0], current_cell[1]]
-                        
+        path_ = [end]
+        cell_ = end
+        while came_from[cell_[0]][cell_[1]] != [-1, -1]:
+            cell_ = came_from[cell[0]][cell[1]]
+            path.append(cell_)
+        path_ = path_[::-1]
+        self._path = path_
+    
+    @property
+    def path(self):
+        return self._path
