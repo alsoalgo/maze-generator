108fd3e189bb28429af3317abab31c68c120f5a4
alsoalgo <gorbulyaalexandr@gmail.com>
2021-04-15 14:17:38 +0300

    DFS algorithm added

--- maze-generator/generator.py
+++ maze-generator/generator.py
@@ -1,4 +1,6 @@
 from maze import *
+from random import randrange
+
 
 class Generator:
     def __init__(self):
@@ -6,6 +8,31 @@ class Generator:
 
     def generate_by_dfs(self, maze):
         size = maze.size
+        used_cells = [[False for i in range(size[1])] \
+                        for i in range(size[0])]
+        stack = []
+        cell = [0, 0]
+        stack.append(cell)
+        used_cells[cell[0]][cell[1]] = True
+        while stack:
+            current_cell = stack.pop()
+            unvisited = False
+            neighbours = maze.get_neighbours(cell)
+            unvisited_neighbours = []
+            for neighbour in neighbours:
+                n_i, n_j = neighbour[0], neighbour[1]
+                if not used_cells[n_i][n_j]:
+                    unvisited = True
+                    unvisited_neighbours.append(neighbour)
+            if unvisited:
+                stack.append(cell)
+                choose = randrange(0, len(unvisited_neighbours))
+                maze.remove_wall(cell, unvisited_neighbours[choose])
+                un_i = unvisited_neighbours[choose][0]
+                un_j = unvisited_neighbours[choose][1]
+                used_cells[un_i][un_j] = True
+                stack.append([un_i, un_j])
+        return maze
 
 
         return maze
