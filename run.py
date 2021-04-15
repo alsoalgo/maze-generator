01a7f8dc57a1cb80cfd82df2e4d3e8eee7c1b7ca
alsoalgo <gorbulyaalexandr@gmail.com>
2021-04-15 21:14:20 +0300

    first working version

--- run.py
+++ run.py
@@ -3,10 +3,16 @@ from maze_generator import *
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("n", help="Rows count")
+    parser.add_argument("m", help="Columns count")
     parser.add_argument("generation_type", help="Select DFS or MST algorithm generation type")
     parser.add_argument("graphics", help="Select console or gui display type")
     args = parser.parse_args()
-    print(args.generation_type, args.graphics)
+    maze_ = Maze(int(args.n), int(args.m))
+    g = Generator()
+    maze_ = g.generate(maze_, args.generation_type)
+    view_ = View(maze_)
+    view_.draw(args.graphics)
 
 
 if __name__ == "__main__":
