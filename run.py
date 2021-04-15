e72796040f6f60214fe8a7fac2072827597eff48
alsoalgo <gorbulyaalexandr@gmail.com>
2021-04-15 20:45:25 +0300

    removed files, changes in run.py

--- run.py
+++ run.py
@@ -2,7 +2,11 @@ import argparse
 from maze_generator import *
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("generation_type", help="Select DFS or MST algorithm generation type")
+    parser.add_argument("graphics", help="Select console or gui display type")
+    args = parser.parse_args()
+    print(args.generation_type, args.graphics)
 
 
 if __name__ == "__main__":
