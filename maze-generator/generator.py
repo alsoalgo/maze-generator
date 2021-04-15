75d79b456a5d7e7865d86d9fbd7425ce17302928
alsoalgo <gorbulyaalexandr@gmail.com>
2021-04-15 18:56:50 +0300

    Working generator.generate_by_mst method

--- maze-generator/generator.py
+++ maze-generator/generator.py
@@ -40,23 +40,24 @@ class Generator:
         all_walls = []
         used_cells = [[False for i in range(size[1])] \
                         for i in range(size[0])]
+        used_cells[0][0] = True
         neighbours = maze.get_neighbours(current_cell)
         for neighbour in neighbours:
-            all_walls.append([current_cell, [neighbour[0], neighbour[1]]]])
+            all_walls.append([current_cell, [neighbour[0], neighbour[1]]])
         while all_walls:
             choose = randrange(0, len(all_walls))
             v = all_walls[choose][0]
             u = all_walls[choose][1]
             is_used_v = int(used_cells[v[0]][v[1]])
             is_used_u = int(used_cells[u[0]][u[1]])
-            if sum(is_used_u + is_used_v) == 1:
+            if is_used_u + is_used_v == 1:
                 if is_used_v:
                     v, u = u, v
                 maze.remove_wall(v, u)
                 used_cells[v[0]][v[1]] = True
                 neighbours = maze.get_neighbours(v)
                 for neighbour in neighbours:
-                    all_walls.append([v, [neighbour[0], neighbour[1]]]])
+                    all_walls.append([v, [neighbour[0], neighbour[1]]])
             all_walls.pop(choose)
         return maze
 
